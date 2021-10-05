a19dbbc9c97ea2efc5faaa742af8f2e99ae40fe3
RYAN NAND <72840724+RyanNand@users.noreply.github.com>
2021-10-05 03:33:06 -0700

    Update Inherit.py

--- PersonalProjects/Python/BasicPythonTutorial/Inherit.py
+++ PersonalProjects/Python/BasicPythonTutorial/Inherit.py
@@ -1,6 +1,6 @@
 from Inherit2 import FavFood
 
-
+# To inherit a class and create a sub-class we must pass the parent class's name as an argument
 class FavIndianFood(FavFood):
 
     def fried(self):  # Same function call has a function from the inherited class
